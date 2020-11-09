4f58366b2b1e07e90477f6013360053b526d932e
JunSeong <ebseud6135@naver.com>
2020-11-09 15:41:39 +0900

    feat: 프로젝트 설정 추가 및 기존 예제 추가

---
+++ src/main/java/blog_example/generics/Generics.java
@@ -0,0 +1,42 @@
+package blog_example.generics;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generics {
+    public static void main(String[] args) {
+        MyOptional<String> stringOptional = new MyOptional<>("hello");
+        List<String> list = new ArrayList<>();
+
+        System.out.println(stringOptional.getTypeName());
+
+    }
+
+}
+
+class MyOptional<T> {
+    T t;
+
+    public MyOptional(T t) {
+        this.t = t;
+    }
+
+    public static <T> MyOptional<T> ofNullable(T t) {
+        return new MyOptional<>(t);
+    }
+
+    public T get() {
+        return this.t;
+    }
+
+    public Type getTypeName() {
+        try {
+            return this.getClass().getDeclaredField("t").getType();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
